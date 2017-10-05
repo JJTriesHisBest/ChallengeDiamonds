b8e99d249bfc9d6bd237395b2dea05174bb64d5d
J J Hassan <jjhassan@protonmail.com>
2017-10-05 08:36:45 +0100

    Remove an unused variable and add some comments

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 I drew some of these out from A - C and counted the number of letters in each diamond. Out of curiosity I then googled that sequence 1, 5, 13 and the first wiki result was **Centered square number**. Pretty cool stuff!
 
+I generated the letters based on their unicode values, and took advantage of the shape's symmetry by using reverse slices to create the rest of the diamond from the top left section.
+
 ## Usage
 ```
 python create_diamond.py [A-Z]
--- create_diamond.py
+++ create_diamond.py
@@ -4,18 +4,25 @@ def create_diamond(letter):
     if(len(letter) != 1):
         print("Too many letters for this here diamond machine. Just use one")
     letter.upper()
-    longest_sequence = ord(letter) - ord('A') + 1
     
     lines = []
+
+    # Create top half of diamond
     for i in range(ord('A'), ord(letter) + 1):
+        # Create a line padded with enough spaces to center align
         line = ' ' * (ord(letter) - i)
+ 
         letters = ""
         for j in range(ord('A'), i + 1):
             letters += chr(j)
+
+        # Add reversed string minus last char
         letters = letters + letters[-2::-1]
         line += letters
+
         lines.append(line)
     
+    # Reverse top half of diamond except the longest row and add to lines
     lines += lines[-2::-1]
     return '\n'.join(lines)
 
