a6b55a9e59a1ac88ae78c6367b5957e3d52ea712
J J Hassan <john.hassan@strath.ac.uk>
2017-10-05 10:10:01 +0100

    Expand test cases

--- test_diamonds.py
+++ test_diamonds.py
@@ -3,3 +3,7 @@ from create_diamond import create_diamond
 def test_correct_diamond():
     b_diamond = " A\nABA\n A"
     assert(create_diamond('B') == b_diamond)
+    a_diamond = "A"
+    assert(create_diamond('A') == a_diamond)
+    c_diamond = "  A\n ABA\nABCBA\n ABA\n  A"
+    assert(create_diamond('C') == c_diamond)
